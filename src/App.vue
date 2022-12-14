8fd887bd736414a8c3de42a7d05909730d655b3f
cosmas <cosmas@localhost.localdomain>
2022-12-14 20:12:33 +0300

    add

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+/* import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'  */
+</script>
+
+<template>
+  <main class=""> 
+      <nav class="flex gap-5 justify-end py-3 text-white bg-green-500">
+    
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> 
+      
+  <RouterView />
+  </main>
+</template>
+
+ 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<template>
+  <main class="">
+  <div>
+    <h1 class="flex justify-center items-center text-6xl">Todo list</h1>
+    <form @submit.prevent="addItem" class="flex flex-col justify-center items-center">
+      <h1 class="text-3xl">add your diary</h1>
+      <input type="text" v-model="newItem" placeholder="Add a new item" required/>
+      <button class="p-5 bg-green-400 rounded-lg">Add your diary</button>
+    </form>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+        <div class="">
+        <button @click="updateItem(index)" class="gap-4 p-5 bg-green-400 rounded-lg">Edit</button>
+        <button @click="deleteItem(index)" class="gap-4 p-5 bg-green-400 rounded-lg">Delete</button>
+      </div>
+      </li>
+    </ul>
+  </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newItem: '',
+      items: []
+    }
+  },
+  methods: {
+    addItem() {
+      this.items.push(this.newItem)
+      this.newItem = ''
+    },
+    updateItem(index) {
+      this.items[index] = prompt('Update item:', this.items[index])
+    },
+    deleteItem(index) {
+      this.items.splice(index, 1)
+    }
+  }
+}
+</script>
+
